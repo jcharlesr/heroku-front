b2aa77de5ac175b2c78d7697650cf2f6e3320825
Juan Riera <jc.rierag@gmail.com>
2018-07-24 20:15:30 -0500

    first commit

---
+++ repaso2/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Tarjeta from './components/Tarjeta/Tarjeta'
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+
+    pokemons:[]
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.pokemons.map((pokemon)=>{
+        console.log("EStoy pintadndo el pokemon "+pokemon);
+        
+        return <Tarjeta nombre={pokemon.name} img={pokemon.img}/>
+      })}
+      </div>
+    );
+  }
+
+  componentDidMount(){
+
+    console.log('Estoy entrando a hacer la peticion');
+    
+
+    //axios.get('http://localhost:5000/pokemons').then((response)=>{
+    axios.get('https://elback.herokuapp.com/pokemons').then((response)=>{
+
+      let data = response.data;
+      console.log(data);
+      
+      this.setState({pokemons:data})
+
+    })
+
+    
+  }
+
+}
+
+export default App;
